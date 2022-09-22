7682aa3465a5250f8316edb0b8aac10fc8ad469d
GersonGoncalves <gerson.devpro@gmail.com>
2022-09-22 08:49:00 -0300

    Criação da pasta styles e arquivo App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/reset.css"
+import "./styles/style.css"
+
+export default function App() {
+    return (
+        <>
+            <div>
+                <h1>Texto para testar aplicação</h1>
+            </div>
+        </>
+    )
+}
